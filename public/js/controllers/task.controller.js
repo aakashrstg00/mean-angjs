7b3cc270924347d00270a26c5e5afa879c574514
Rastogi <rasaakas@38f9d368e316.ant.amazon.com>
2020-04-24 19:44:42 +0530

    added undo delete feature

--- public/js/controllers/task.controller.js
+++ public/js/controllers/task.controller.js
@@ -1,6 +1,12 @@
-app.controller("TaskController", function ($scope, $timeout, TaskService) {
+app.controller("TaskController", function ($scope, $timeout, $interval, TaskService) {
     $scope.taskAdded = false;
     $scope.taskDeleted = false;
+    $scope.undoDelete = false;
+    $scope.undoDeletePressed = false;
+    $scope.hideAllDeleteButtons = false;
+    var undoTimer;
+    var undoTimerInterval
+
     $scope.loadTasks = function () {
         var promise = TaskService.getTasks();
         promise.then(function (data) {
@@ -9,6 +15,7 @@ app.controller("TaskController", function ($scope, $timeout, TaskService) {
             $scope.error = error;
         });
     };
+
     $scope.addTask = function (event) {
         event.preventDefault();
         var newTask = {
@@ -28,11 +35,31 @@ app.controller("TaskController", function ($scope, $timeout, TaskService) {
             $scope.error = error;
         });
     };
+
     $scope.deleteTask = function(id){
-        var task = $scope.tasks;
+        $scope.deleteTimerTime = 3; //seconds
+
+        $scope.hideAllDeleteButtons = true;
+        $scope.undoDelete = true;
+        undoTimer = $timeout(()=>{
+            $scope.undoDelete = false;
+            $scope.undoDeletePressed = false;
+            $scope.actuallyDeleteTask(id);
+        }, $scope.deleteTimerTime * 1000 - 200);
+        undoTimerInterval = $interval(()=>{
+            $scope.deleteTimerTime--;
+            if($scope.deleteTimerTime == 0) $interval.cancel(undoTimerInterval);
+        },1000)
+    };
+    $scope.taskDeleted = false;
+    $scope.undoDelete = false;
+    $scope.undoDeletePressed = false;
+    $scope.hideAllDeleteButtons = false;
+    $scope.actuallyDeleteTask = (id)=>{
         var promise = TaskService.deleteTask(id);
         promise.then(function (data) {
             $scope.taskDeleted = true;
+            $scope.hideAllDeleteButtons = false;
             console.log($scope.tasks);
             $scope.loadTasks();
             $timeout(function () {
@@ -41,7 +68,7 @@ app.controller("TaskController", function ($scope, $timeout, TaskService) {
         }, function (error) {
             $scope.error = error;
         });
-    };
+    }
     $scope.updateStatus = function(task){
         var t = {
             _id: task._id,
@@ -55,5 +82,15 @@ app.controller("TaskController", function ($scope, $timeout, TaskService) {
             $scope.error = error;
         });
     };
+    $scope.undoDeleteTask = ()=>{
+        $scope.undoDeletePressed = true;
+        
+        $timeout.cancel(undoTimer);
+        $interval.cancel(undoTimerInterval);
+        
+        $scope.undoDelete = false;
+        $scope.undoDeletePressed = false;
+        $scope.hideAllDeleteButtons = false;
+    }
     $scope.loadTasks();
 });
